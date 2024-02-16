47648eb7fa443493362adeb9d6ad0945abbb8359
Calvin <calvinlu@live.com>
2024-02-16 11:03:47 -0800

    what is with the tabs

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -7,13 +7,25 @@ public class Digits
 
 	public Digits(int num)
 	{ /* to be implemented in part (a) */ 
-		
+		digitList = new ArrayList<>();
+		String s = String.valueOf(num);
+	    	for (int i = 0; i < s.length(); i++) {
+	      		digitList.add(Integer.parseInt(s.substring(i, i + 1)));
+		}
 	}
 
 	public boolean isStrictlyIncreasing()
 	{ /* to be implemented in part (b) */
-		
-
+		int last = digitList.get(0);
+	    	for (int i = 1; i < digitList.size(); i++) {
+	     	if (last >= digitList.get(i)) {
+			return false;
+	      	}
+	
+	      	last = digitList.get(i);
+    		}
+	
+	    return true;
 	}
 	
 	public String toString()
